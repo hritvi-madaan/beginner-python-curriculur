32504829863a2a060b819f6d4f8601487b612d7f
hritvi-madaan <pushek.madaan+hritvi@gmail.com>
2026-05-10 01:01:17 +0000

    finished lesson 1

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,6 @@
+print("Hello world")
+print("My name is Hritvi")
+print('My favorite food is pizza!')
+print("I love earth")
+#Error:mismatched qotation marks
+#print("I love dogs")
---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,10 @@
+x = 5 # integer:whole  number which is + or -
+print(x)
+
+number = "3.97" # Floats: number with a decimel point
+print(number)
+
+favorite_food- food = "pizza" # string: text enclosed in qoutes
+print("My favorite food is", favorite_food)
+
+print(4.21"dogs", x)
